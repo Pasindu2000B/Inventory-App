fd3d86edb672105a783330535213a4615427cc92
PasinduRi <bimsarapasindu714@gmail.com.com>
2024-06-28 14:46:03 +0530

    Name Change

--- mongodb.js
+++ mongodb.js
@@ -1,42 +1,43 @@
-require('dotenv').config()
+require("dotenv").config();
 
 const mongoose = require("mongoose");
 
 const mongoURI = process.env.MONGODB_URI;
 
-mongoose.connect(mongoURI)
-.then(() => {
-  console.log("DB connected");
-})
-.catch((error) => {
-  console.error("DB failed to connect:", error);
-});
+mongoose
+  .connect(mongoURI)
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch((error) => {
+    console.error("DB failed to connect:", error);
+  });
 
 const LogInSchema = new mongoose.Schema({
   firstName: {
     type: String,
-    required: true
+    required: true,
   },
   lastName: {
     type: String,
-    required: true
+    required: true,
   },
   mobileNo: {
     type: String,
-    required: true
+    required: true,
   },
   email: {
     type: String,
-    required: true
+    required: true,
   },
   password: {
     type: String,
-    required: true
+    required: true,
   },
   profilePicture: {
     type: String,
-    required: false
-  }
+    required: false,
+  },
 });
 
 const LogInCollection = mongoose.model("LogInCollection", LogInSchema);
